79a54c9f9cfc96421b81586611f1b3497a21fc8f
Mayank Singh <mayanksingh8005@gmail.com>
2018-09-07 07:12:35 +0530

    loader files

--- src/youtubedl.js
+++ src/youtubedl.js
@@ -1,4 +1,13 @@
 
-
-var videoUrls=ytplayer.config.args.url_encoded_fmt_stream_map.split(",");
-console.log("Our extension has loaded");
+/** created by mayank */
+window.onload=function()
+{
+    var videoUrls=window.ytplayer.config.args.url_encoded_fmt_stream_map.split(",").map(function(item){
+        return item.split("&").reduce(function(pre,cur){
+            console.log(pre,cur);
+            cur=cur.split("=");
+            return Object.assign(pre, {[cur[0]]: decodeURIComponent(cur[1])});
+        },{});
+    });
+    console.log("Our extension has loaded",videoUrls);
+}
